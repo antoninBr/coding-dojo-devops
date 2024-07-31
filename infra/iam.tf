1439e8230a901b7b8af95985d63f2b0397cfca44
Antonin BRUGNOT <antonin.brugnot@laposte.net>
2024-07-31 14:26:58 +0000

    Change IAM

--- infra/iam.tf
+++ infra/iam.tf
@@ -1,5 +1,7 @@
-resource "google_project_iam_member" "run_invoker" {
-  project = var.project_id
-  role    = "roles/run.invoker"
-  member  = "allUsers"
+resource "google_cloud_run_service_iam_member" "member" {
+  location = google_cloud_run_service.default.location
+  project  = google_cloud_run_service.default.project
+  service  = google_cloud_run_service.default.name
+  role     = "roles/run.invoker"
+  member   = "allUsers"
 }
