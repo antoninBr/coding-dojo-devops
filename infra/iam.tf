aa05d5dd8fbc361f39a4ae5e203b57a446839420
Antonin BRUGNOT <antonin.brugnot@laposte.net>
2024-07-31 14:59:43 +0000

    IAM changement nom du service

--- infra/iam.tf
+++ infra/iam.tf
@@ -1,7 +1,7 @@
 resource "google_cloud_run_service_iam_member" "member" {
   location = var.region
   project  = var.project_id
-  service  = "default"
+  service  = "flask-minesweeper"
   role     = "roles/run.invoker"
   member   = "allUsers"
 }
